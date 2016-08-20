87b1843e84fb67ae2a84adac86b53f186a286ee5
Justin <jlewsader@gmail.com>
2016-08-19 22:22:55 -0400

    formatted to print Name and Price only

--- data_helper.js
+++ data_helper.js
@@ -4,6 +4,14 @@ var dataEndpoint = 'https://www.quandl.com/api/v3/datasets/COM/CORN_2.json?limit
 
 request(dataEndpoint, function(error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body)
+    var contents = body;
+    var jsonData = JSON.parse(contents);
+    console.log(jsonData.dataset.name);
+    // console.log(jsonData.dataset.data[0]);
+    var obj = jsonData.dataset.data[0];
+    var priceSet = JSON.stringify(obj);
+    var price = priceSet.split(",");
+    var price = price[1].split("]");
+    console.log(price[0]);
   };
 })

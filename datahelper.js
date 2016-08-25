8477bbafaee50d0d0f82a523adb00e9a4a397edd
jlewsader <jlewsader@gmail.com>
2016-08-25 12:05:10 -0500

    starting to work on formatData function in datahelper.js

--- datahelper.js
+++ datahelper.js
@@ -27,29 +27,30 @@ DataHelper.prototype.getStatus = function() {
   return rp(options);
 };
 
-// DataHelper.prototype.formatAirportStatus = function(airportStatus) {
-//   var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.')({
-//     weather: airportStatus.weather.weather,
-//     temp: airportStatus.weather.temp,
-//     wind: airportStatus.weather.wind
-//   });
-//   if (airportStatus.delay === 'true') {
-//     var template = _.template('There is currently a delay for ${airport}. ' +
-//       'The average delay time is ${delay_time}. ' +
-//       'Delay is because of the following: ${delay_reason}. ${weather}');
-//     return template({
-//       airport: airportStatus.name,
-//       delay_time: airportStatus.status.avgDelay,
-//       delay_reason: airportStatus.status.reason,
-//       weather: weather
-//     });
-//   } else {
-//     //no delay
-//     return _.template('There is currently no delay at ${airport}. ${weather}')({
-//       airport: airportStatus.name,
-//       weather: weather
-//     });
-//   }
-// };
+DataHelper.prototype.formatData = function(dataJSON) {
+  var template = _.template('The current local grain bids are, ${company}, ${symbol}, $${price}')({
+    company: dataJSON.results[0].company,
+    symbol: dataJSON.results[0].bids[0].commodity,
+    price: dataJSON.results[0].bids[0].price
+  });
+  return template
+  // if (airportStatus.delay === 'true') {
+  //   var template = _.template('There is currently a delay for ${airport}. ' +
+  //     'The average delay time is ${delay_time}. ' +
+  //     'Delay is because of the following: ${delay_reason}. ${weather}');
+  //   return template({
+  //     airport: airportStatus.name,
+  //     delay_time: airportStatus.status.avgDelay,
+  //     delay_reason: airportStatus.status.reason,
+  //     weather: weather
+  //   });
+  // } else {
+  //   //no delay
+  //   return _.template('There is currently no delay at ${airport}. ${weather}')({
+  //     airport: airportStatus.name,
+  //     weather: weather
+  //   });
+  // }
+};
 
 module.exports = DataHelper;
--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var DataHelper = require('./datahelper.js');
 
 var dataHelper = new DataHelper();
-dataHelper.requestStatus().then(function(data){
-  console.log(data.results[0].bids[0].price);
-}
-)
+dataHelper.requestStatus().then(function(dataJSON){
+  console.log(dataHelper.formatData(dataJSON));
+});
